65a56032a88d52071069086cf60fdc9658ebc11c
Glenn Gruenberg <glenn@gruenberg.ch>
2019-10-18 09:30:14 +0200

    Rename option operator parameter to signal it being ignored

--- Option/Option.cs
+++ Option/Option.cs
@@ -9,7 +9,7 @@ namespace Option
             ? (Option<T>)None.Value
             : new Some<T>(value);
 
-        public static implicit operator Option<T>(None none) => new None<T>();
+        public static implicit operator Option<T>(None _) => new None<T>();
 
         public abstract Option<TResult> Apply<TResult>(Func<T, TResult> function);
 
